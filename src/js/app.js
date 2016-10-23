f60681233015c9c025079da46cfccaac4aae8cea
Louis Bourque <louisrbourque@gmail.com>
2016-10-23 14:22:42 -0600

    Add gun database, update background colour

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
-var app = app || {};
+import Gun from 'gun/gun';
+
+const gun = Gun(['https://gundb-louis.herokuapp.com/gun']);
+
+
+const textInput = document.getElementById('myInput');
+
+var ref = gun.get('text').not(function (key) {
+  // put in an object and key it
+  gun.put({
+    text: ''
+  }).key(key)
+});
+ref.on(function(data){
+  textInput.value = data.text;
+  document.body.style.backgroundColor = data.text;
+});
+
+window.updateMyInput = () => {
+  ref.path('text').put(textInput.value);
+}
